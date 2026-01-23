f9caf89d256a1a3499df254e9bc9fa9209c48eee
Ewing <cyyooooo00@gmail.com>
2026-01-23 03:04:55 +0000

    fix: torch.load set weights_only=False

--- fireredasr/models/fireredasr.py
+++ fireredasr/models/fireredasr.py
@@ -107,7 +107,7 @@ class FireRedAsr:
 
 
 def load_fireredasr_aed_model(model_path):
-    package = torch.load(model_path, map_location=lambda storage, loc: storage)
+    package = torch.load(model_path, map_location=lambda storage, loc: storage, weights_only=False)
     print("model args:", package["args"])
     model = FireRedAsrAed.from_args(package["args"])
     model.load_state_dict(package["model_state_dict"], strict=True)
